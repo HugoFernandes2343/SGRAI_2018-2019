d05a6917edaf2b53f2bca3f9c38ff22970fd0940
MarcoCarneiro <1160777@isep.ipp.pt>
2018-11-12 22:31:14 +0000

    Added some methods

--- bundle.js
+++ bundle.js
@@ -48269,21 +48269,50 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(2, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+//Cube1
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+//Cube2
+var geometry2 = new THREE.BoxGeometry(1, 1, 1);
+var material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+var cube2 = new THREE.Mesh(geometry2, material2);
+scene.add(cube2);
+
+//Cube3
+var geometry3 = new THREE.BoxGeometry(0.05, 0.05, 0.05);
+var material3 = new THREE.MeshBasicMaterial({ color: 0xee2a00, wireframe: true });
+var cube3 = new THREE.Mesh(geometry3, material3);
+scene.add(cube3);
+
 camera.position.z = 2;
 
-var animate = function () {
-    requestAnimationFrame(animate);
+function posFigure(figure, posX, posY, rotX, rotY) {
+    figure.position.x = posX;
+    figure.position.y = posY;
+    figure.rotation.x += rotX;
+    figure.rotation.y += rotY;
+}
 
-    cube.rotation.x += 0.1;
-    //cube.rotation.y += 0.01;
-
+//draw scene
+var render = function () {
     renderer.render(scene, camera);
-};
+}
 
-animate();
+var update = function () {
+}
+
+var loop = function () {
+    requestAnimationFrame(loop);
+    posFigure(cube, 1, 0, 0.05, 0.05);
+    posFigure(cube2, -1, 0, 1, 1);
+    posFigure(cube3, 0, 0, -0.05, -0.05);
+    update();
+    render();
+}
+
+loop();
+
 },{"./js/three":1}]},{},[2]);
