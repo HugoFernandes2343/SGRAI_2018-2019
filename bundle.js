ef40e74c87b9affd5ba50180730312c817f1d6b6
MarcoCarneiro <1160777@isep.ipp.pt>
2018-11-12 15:37:02 +0000

    Changed some settings

--- bundle.js
+++ bundle.js
@@ -48270,17 +48270,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(2, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 2;
 
 var animate = function () {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.rotation.x += 0.1;
+    //cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 };
